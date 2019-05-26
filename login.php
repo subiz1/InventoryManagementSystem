3ab297cd6411e92f1b51f3f3e8157cc96ef2311f
subhan anjum <subiz0@hotmail.com>
2019-05-26 13:24:52 +0500

    First Commit

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Login | IMS
+        </title>
+        <script scr="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <script scr="js/jquery-3.4.1.min.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
